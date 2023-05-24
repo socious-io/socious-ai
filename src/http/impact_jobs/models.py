777e454721570dd16e35ac7139d019412daafc23
jeyem <me@e-mahmoudi.me>
2023-05-24 22:54:39 +0330

    conditional org name

--- src/http/impact_jobs/models.py
+++ src/http/impact_jobs/models.py
@@ -5,6 +5,7 @@ from src.db import DB
 jobs = DB.fetch_lazy('''
   SELECT p.title, p.description, org.name as org_name, org.description as org_description 
   FROM projects p join organizations org on org.id=p.identity_id
+  WHERE org.name is not null
 ''')
 
 
