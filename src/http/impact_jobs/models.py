d03755b14c23ed926a988108470990637c5ab5ab
jeyem <me@e-mahmoudi.me>
2023-06-27 13:19:17 +0330

    fix input

--- src/http/impact_jobs/models.py
+++ src/http/impact_jobs/models.py
@@ -6,7 +6,7 @@ jobs = DB.fetch_lazy('''
   SELECT p.title, p.description, org.name as org_name, org.description as org_description 
   FROM projects p join organizations org on org.id=p.identity_id
   WHERE org.name IS NOT NULL OR org.name <> '' ORDER BY p.created_at
-''')
+''', limit=10000)
 
 
-impact_detector = ImpactDetector(jobs, 10000)
+impact_detector = ImpactDetector(jobs)
