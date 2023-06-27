dca4c5cfd0d63546da8b0b3eefd8e04585019ea9
jeyem <me@e-mahmoudi.me>
2023-06-27 13:14:06 +0330

    fix query

--- src/http/impact_jobs/models.py
+++ src/http/impact_jobs/models.py
@@ -5,7 +5,7 @@ from src.db import DB
 jobs = DB.fetch_lazy('''
   SELECT p.title, p.description, org.name as org_name, org.description as org_description 
   FROM projects p join organizations org on org.id=p.identity_id
-  WHERE org.name IS NOT NULL OR org.name <> '' ORDER BY created_at
+  WHERE org.name IS NOT NULL OR org.name <> '' ORDER BY p.created_at
 ''')
 
 
